47670b383bb28a73d1e7e96578fe56e6f3b7cc1a
Jason Chen <447462131@qq.com>
2024-07-04 17:38:46 +0800

    feat: 合约联调

--- src/views/QdayStaking/TotalCard.tsx
+++ src/views/QdayStaking/TotalCard.tsx
@@ -95,7 +95,7 @@ export default function TotalCard() {
   });
   return (
     <div className="flex  h-120px font-size-12px gap-10px">
-      <div className="flex-1 w-20% h-100% flex flex-col justify-between">
+      <div className="flex-1 w-33% h-100% flex flex-col justify-between">
         <div className="flex flex-col justify-between">
           <div>全网质押veQday</div>
           <div>{totalStake}QDAY</div>
@@ -111,22 +111,15 @@ export default function TotalCard() {
         </div>
       </div>
 
-      <div className="flex-1 w-20% h-100% flex flex-col justify-between">
+      <div className="flex-1 w-33% h-100% flex flex-col justify-between">
         <div className="flex flex-col justify-between">
           <div>我的质押veQday</div>
           <div>{userStakeVal}QDAY</div>
         </div>
-        <div className="flex flex-col justify-between">
+        {/* <div className="flex flex-col justify-between">
           <div>我的收益</div>
           <div>10000QDAY</div>
-        </div>
-      </div>
-
-      <div className="flex-1 w-20% h-100% flex flex-col justify-between">
-        <div className="flex flex-col justify-between">
-          <div>全网收益</div>
-          <div>10000QDAY</div>
-        </div>
+        </div> */}
         <div className="flex flex-col justify-between">
           <div>待提现奖金</div>
           <div>
@@ -137,7 +130,8 @@ export default function TotalCard() {
           </div>
         </div>
       </div>
-      <div className="flex-1 w-20% h-100% flex flex-col justify-between">
+
+      <div className="flex-1 w-33% h-100% flex flex-col justify-between">
         <div className="flex flex-col justify-between">
           <div>我的余额</div>
           <div>{balance} QDAY</div>
